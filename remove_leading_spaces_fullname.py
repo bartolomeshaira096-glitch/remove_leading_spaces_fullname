ef1a68112dac6d5af4609d2dbe65779b2f457eb2
unknown <bartolomeshaira096@gmail.com$ git config --global user.email bartolomeshaira096@gmail.com>
2026-03-09 20:33:58 +0800

    adding a second code to next psuedocode which is to remove a speaces at the beginning, adding more spaces at the first code which is the fullname

--- remove_leading_spaces_fullname.py
+++ remove_leading_spaces_fullname.py
@@ -1,5 +1,5 @@
 # Ask the user to input their fullname with spaces at the beginning
-fullname = input("Enter your fullname: ")
-
+fullname = input("Enter your fullname:     ")
 # Remove the spaces at the beginning
+clean_name = fullname.lstrip()
 # Remove the spaces at the beginning 
