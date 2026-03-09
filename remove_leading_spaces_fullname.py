61dc8a07761ae3c8d356528050fb2356ddc843d7
unknown <bartolomeshaira096@gmail.com$ git config --global user.email bartolomeshaira096@gmail.com>
2026-03-09 20:28:02 +0800

    adding a code for my first psuedocode which is the fullname, my file name is incorrect because it doesn't have a .py so i changed it into .py

--- remove_leading_spaces_fullname.py
+++ remove_leading_spaces_fullname.py
@@ -1,3 +1,5 @@
 # Ask the user to input their fullname with spaces at the beginning
+fullname = input("Enter your fullname: ")
+
 # Remove the spaces at the beginning
 # Remove the spaces at the beginning 
